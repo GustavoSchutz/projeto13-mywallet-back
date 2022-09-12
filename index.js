0c72bdb755c665e06853445e1b58b0994bf474a4
GustavoSchutz <gutofelipe8@gmail.com>
2022-09-12 00:00:02 -0300

    build: dotenv; mongodb; joi; dayjs;

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import joi from 'joi';
+import dayjs from 'dayjs';
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
